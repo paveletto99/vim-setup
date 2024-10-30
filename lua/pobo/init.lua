65a4d35f8f5bdb362dd52bec193ea0dd70d1a10a
pobo <paveletto99@gmail.commit>
2024-10-30 11:46:58 +0100

    feat: telescope ignore pattern

--- lua/pobo/init.lua
+++ lua/pobo/init.lua
@@ -9,24 +9,24 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+  require("plenary.reload").reload_module(name)
 end
 
 autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
 })
 
 autocmd({ "BufWritePre" }, {
-    group = PoboGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+  group = PoboGroup,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
 })
 
 vim.g.netrw_browse_split = 0
@@ -35,24 +35,33 @@ vim.g.netrw_winsize = 25
 
 
 autocmd("BufWritePre", {
-    pattern = "*.go",
-    callback = function()
-      local params = vim.lsp.util.make_range_params()
-      params.context = {only = {"source.organizeImports"}}
-      -- buf_request_sync defaults to a 1000ms timeout. Depending on your
-      -- machine and codebase, you may want longer. Add an additional
-      -- argument after params if you find that you have to write the file
-      -- twice for changes to be saved.
-      -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-      local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-      for cid, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-          if r.edit then
-            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-            vim.lsp.util.apply_workspace_edit(r.edit, enc)
-          end
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
         end
       end
-      vim.lsp.buf.format({async = false})
     end
-  })
+    vim.lsp.buf.format({ async = false })
+  end
+})
+
+require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = {
+      "node_modules",
+      "vendor"
+    }
+  }
+}
